9d86a3f1cf0c2c82c5eddc1e426b51d1709fa4ce
phavanee <147310184+phavanee@users.noreply.github.com>
2025-12-12 14:15:22 +0800

    minor changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -145,7 +145,7 @@
                         </div>
                         <a href="{{ route('classrooms.index') }}"
                             class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('classrooms.*') ? 'border-blue-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium leading-5 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            {{ __('My Classrooms') }}
+                            {{ __('Kelas Saya') }}
                         </a>
                     @else
                         <a href="{{ route('lesson.index') }}"
@@ -316,7 +316,7 @@
                 </a>
                 <a href="{{ route('classrooms.index') }}"
                     class="block px-4 py-2 text-base font-medium {{ request()->routeIs('classrooms.*') ? 'text-gray-900 bg-gray-100' : 'text-gray-500 hover:text-gray-800 hover:bg-gray-100' }} transition duration-150 ease-in-out">
-                    {{ __('My Classrooms') }}
+                    {{ __('Kelas Saya') }}
                 </a>
             @else
                 <a href="{{ route('lesson.index') }}"
